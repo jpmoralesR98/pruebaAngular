e472f4d21b8b323c98d0f6952d4df08615150fea
jpmoralesR98 <damianytutoriales@gmail.com>
2024-05-15 14:45:59 -0500

    Add files via upload

---
+++ app.component.ts
@@ -0,0 +1,24 @@
+import {Component, OnInit} from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent implements OnInit{
+  title = 'postABC';
+
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+
+
+  }
+
+}
